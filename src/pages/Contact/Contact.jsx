cfd707dc88ed29f1c582349e943e885072524696
tamsoul <tamsoul>
2024-03-05 22:12:38 +0700

    contact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -51,34 +51,33 @@ const Contact = () => {
 
     return (
         <Helmet title="Contact">
-            <section>
-                <CommonSection title="Contact" />
-                <Container>
-                    <Row>
-                        <h3 className={cx('contact__title')}>Contact form</h3>
-                        <div className={cx('container')}>
-                            <form action="">
-                                <label htmlFor="fname">First Name</label>
-                                <input
-                                    className={cx('contact__input')}
-                                    type="text"
-                                    id="fname"
-                                    name="fname"
-                                    value={user.fname}
-                                    onChange={getDataFromUser}
-                                    placeholder="Your name..."
-                                />
-                                <label htmlFor="lname">Last Name</label>
-                                <input
-                                    className={cx('contact__input')}
-                                    type="text"
-                                    id="lname"
-                                    name="lname"
-                                    value={user.lname}
-                                    onChange={getDataFromUser}
-                                    placeholder="Your last name..."
-                                />
-                                {/* <label htmlFor="email">Email</label>
+            <CommonSection title="Contact" />
+            <Container>
+                <Row>
+                    <h3 className={cx('contact__title')}>Contact form</h3>
+                    <div className={cx('container')}>
+                        <form action="">
+                            <label htmlFor="fname">First Name</label>
+                            <input
+                                className={cx('contact__input')}
+                                type="text"
+                                id="fname"
+                                name="fname"
+                                value={user.fname}
+                                onChange={getDataFromUser}
+                                placeholder="Your name..."
+                            />
+                            <label htmlFor="lname">Last Name</label>
+                            <input
+                                className={cx('contact__input')}
+                                type="text"
+                                id="lname"
+                                name="lname"
+                                value={user.lname}
+                                onChange={getDataFromUser}
+                                placeholder="Your last name..."
+                            />
+                            {/* <label htmlFor="email">Email</label>
                             <input
                                 className={cx('contact__input')}
                                 type="text"
@@ -88,41 +87,40 @@ const Contact = () => {
                                 onChange={getDataFromUser}
                                 placeholder="Your email..."
                             /> */}
-                                <label htmlFor="country">Country</label>
-                                <select
-                                    className={cx('contact__select')}
-                                    name="country"
-                                    id="country"
-                                    value={user.country}
-                                    onChange={getDataFromUser}
-                                >
-                                    <option>Select country</option>
-                                    <option value="vietnam">VietNam</option>
-                                    <option value="usa">USA</option>
-                                    <option value="canada">Canada</option>
-                                </select>
-                                <label htmlFor="subject">Message</label>
-                                <textarea
-                                    className={cx('contact__textarea')}
-                                    name="subject"
-                                    id="subject"
-                                    value={user.subject}
-                                    placeholder="Write something..."
-                                    style={{ height: 200 }}
-                                    // defaultValue={''}
-                                    onChange={getDataFromUser}
-                                />
-                                <input
-                                    className={cx('contact__submit')}
-                                    type="submit"
-                                    defaultValue="Submit"
-                                    onClick={postData}
-                                ></input>
-                            </form>
-                        </div>
-                    </Row>
-                </Container>
-            </section>
+                            <label htmlFor="country">Country</label>
+                            <select
+                                className={cx('contact__select')}
+                                name="country"
+                                id="country"
+                                value={user.country}
+                                onChange={getDataFromUser}
+                            >
+                                <option>Select country</option>
+                                <option value="vietnam">VietNam</option>
+                                <option value="usa">USA</option>
+                                <option value="canada">Canada</option>
+                            </select>
+                            <label htmlFor="subject">Message</label>
+                            <textarea
+                                className={cx('contact__textarea')}
+                                name="subject"
+                                id="subject"
+                                value={user.subject}
+                                placeholder="Write something..."
+                                style={{ height: 200 }}
+                                // defaultValue={''}
+                                onChange={getDataFromUser}
+                            />
+                            <input
+                                className={cx('contact__submit')}
+                                type="submit"
+                                defaultValue="Submit"
+                                onClick={postData}
+                            ></input>
+                        </form>
+                    </div>
+                </Row>
+            </Container>
         </Helmet>
     );
 };
